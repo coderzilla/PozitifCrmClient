643d6b87168e270acc02da93a312c0e04faa9389
TURGUT UMEÇ <turgutumec@gmail.com>
2015-02-11 19:50:34 +0200

    Guest

--- Angularjs-Signalr-App/Scripts/Chat/ChatController.js
+++ Angularjs-Signalr-App/Scripts/Chat/ChatController.js
@@ -1,19 +1,52 @@
 ﻿
 globalmodule.controller('mainCtrl', function ($scope, ChatService) {
+   
     var hubproxy = ChatService;
     ChatService.connOpen;
     $scope.messages = [];
-    $scope.showLogin = true;
-
+    $scope.showLogin = true; 
+    var positions;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showlocation);
+    }
+    var ip = "";
+    $.get('http://jsonip.com', function (res) {ip = res.ip; });
+    function showlocation(ps) { 
+        positions = ps;
+    }
     $scope.ChatStart = function () {
-        $scope.User.SignalRId = ChatService.conn_Id();
-        $.post("http://localhost:1581/", { User: $scope.User }, function (data) {
+        //$scope.User.SignalRId = ChatService.conn_Id();
+     
 
-        });
-        if ($scope.User.Name.length > 3) {
-            $scope.showLogin = false;
-            $scope.showChat = true;
-        }
+       
+        $scope.User.IpAddress = ip;
+        $scope.User.UserUniqueIdentifier = null;
+        
+        $scope.User.InboundUrl = window.location.href;
+        var now = new Date();
+        $scope.User.LastConnectionDate = now.toLocaleString();
+        $scope.User.SkillId = null;
+        $scope.User.UserAgent = null; 
+        $scope.User.Latitude = positions.coords.latitude.toString();
+        $scope.User.Longitude = positions.coords.longitude.toString(); 
+        $.post("http://localhost:1581/Guests/Create", { guest: $scope.User }, function (data) {
+             
+              
+        })
+          //.done(function () {
+          //    $scope.showLogin = false;
+          //    $scope.showChat = true;
+          //})
+          //.fail(function () {
+          //    console.log("error");
+          //})
+          //.always(function () {
+          //    console.log("finished");
+        //});
+
+        $scope.showLogin = false;
+        $scope.showChat = true;
+        
     }
 
     $scope.sendMessage = function () {
@@ -23,14 +56,13 @@ globalmodule.controller('mainCtrl', function ($scope, ChatService) {
     }
 
     $scope.EnterSend = function (keykode) {
-        if (keykode=="13" && $scope.User.Message.length > 0) {
+        if (keykode == "13" && $scope.User.Message.length > 0) {
             sendmsg();
         }
     }
 
 
-    function sendmsg()
-    {
+    function sendmsg() {
         hubproxy.setData($scope.User);
         $scope.User.Message = "";
     }
